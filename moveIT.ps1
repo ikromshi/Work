b7ee9fa0132d92e877e5bfd4c11a956057c2abf1
Ikrom Numonov <inumonov@ithaca.edu>
2022-09-13 17:28:19 -0400

    path is wrong

--- moveIT.ps1
+++ moveIT.ps1
@@ -1,2 +1,6 @@
-Copy-Item -Path ".\Bookmarks" -Destination "$env:USERPROFILE/AppData/Local/Google/Chrome/User Data/Default" -Recurse -force
-Copy-Item -Path ".\Bookmarks" -Destination "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default" -Recurse -force
+# Mounting the Service Desk network drive as "W:"
+#net use W: "\\neo\Tech_Renewal\Service Desk"
+
+Copy-Item -Path "\\neo\Tech_Renewal\Service Desk\Bookmarks\" -Destination "$env:USERPROFILE/AppData/Local/Google/Chrome/User Data/Default" -Recurse -force
+Copy-Item -Path "\\neo\Tech_Renewal\Service Desk\Bookmarks\" -Destination "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default" -Recurse -force
+Copy-Item -Path "$env:USERPROFILE/AppData/Local/Google/Chrome/User Data/Default/" .\Bookmarks
