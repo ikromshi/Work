2ed6e5a0a0b9347ce6f8d71687c1247b4107231c
Ikrom Numonov <inumonov@ithaca.edu>
2022-09-15 09:55:17 -0400

    complete; now copies the file without mounting the network drive first

--- moveIT.ps1
+++ moveIT.ps1
@@ -1,6 +1,15 @@
 # Mounting the Service Desk network drive as "W:"
 #net use W: "\\neo\Tech_Renewal\Service Desk"
 
-Copy-Item -Path "\\neo\Tech_Renewal\Service Desk\Bookmarks\" -Destination "$env:USERPROFILE/AppData/Local/Google/Chrome/User Data/Default" -Recurse -force
-Copy-Item -Path "\\neo\Tech_Renewal\Service Desk\Bookmarks\" -Destination "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default" -Recurse -force
-Copy-Item -Path "$env:USERPROFILE/AppData/Local/Google/Chrome/User Data/Default/" .\Bookmarks
+Copy-Item -Path "\\neo\Tech_Renewal\Service Desk\Bookmarks\Bookmarks" -Destination "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default" -Recurse -force
+Copy-Item -Path "\\neo\Tech_Renewal\Service Desk\Bookmarks\Bookmarks" -Destination "$env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default" -Recurse -force
+# Move-Item -Path "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks\Bookmarks" -Destination ".."
+
+# Get-ChildItem -Path "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks\" -Recurse -File | Move-Item -Destination "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\"
+
+
+# mv "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks\" "..\AppData\Local\Google\Chrome\User Data\Default\"
+
+
+
+# mv "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Bookmarks" "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\"
